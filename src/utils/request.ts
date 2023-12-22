18c23f4cde20874b912525226f7e4181646fa036
code-lucky <2924249993@qq.com>
2023-12-22 19:02:45 +0800

    test CI

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -10,8 +10,8 @@ export const request = (options: any) => {
     // create an axios instance
     const service = axios.create({
       // baseURL: process.env.BASE_API, // api 的 base_url
-      // baseURL:'https://api.server-api.cn',
-      baseURL:'http://124.222.178.74:3000',
+      baseURL:'https://api.server-api.cn',
+      // baseURL:'http://124.222.178.74:3000',
       // baseURL: '/api',
       timeout: 80000 // request timeout
     })
